1a16308cd2e65c30d664bae6b701ecd328e3c731
maxim <borisov130490@gmail.com>
2025-11-23 21:15:07 +0300

    change block

--- components/pages/hero-page-components/sale-products-section/sale-products-section.tsx
+++ components/pages/hero-page-components/sale-products-section/sale-products-section.tsx
@@ -3,6 +3,8 @@ import BaseContainer from "@/components/general/containers/base-container";
 import { cn } from "@/lib/utils";
 
 import { Product } from "@/types/catalog";
+import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 interface SaleProductsSectionProps
   extends React.HTMLAttributes<HTMLDivElement> {
@@ -20,6 +22,15 @@ const SaleProductsSection: React.FC<SaleProductsSectionProps> = ({
       <h3 className="text-xl font-bold">{title}</h3>
       <div className="relative z-10 w-[93%] sm:w-full mt-4">
         <ProductsSaleCarousel products={products} titleBlock={title} />
+      </div>
+      <div className="flex gap-2 items-center text-primary mt-6">
+        <Link
+          className="text-sm font-bold"
+          href={"/catalog/kondczionery/split-sistemy"}
+        >
+          В каталог
+        </Link>
+        <ArrowLongRightIcon className="size-5" />
       </div>
     </BaseContainer>
   );
